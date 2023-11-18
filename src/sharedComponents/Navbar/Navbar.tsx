370fddb23bf1891c915ccf07dfd920e4871c2fd8
butadpj <butadpj@gmail.com>
2023-11-18 19:58:02 +0800

    Adjust styles on medium-large screens

--- src/sharedComponents/Navbar/Navbar.tsx
+++ src/sharedComponents/Navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { createEffect, createSignal, onMount } from "solid-js";
+import { createSignal, onMount } from "solid-js";
 import Content from "./components/Content";
 import {
   FaBrandsDev,
@@ -49,18 +49,19 @@ export default function Navbar() {
 
   return (
     <>
-      <nav class="navbar glassmorphism fixed z-30 flex w-full items-center justify-between border-b border-b-primary bg-background px-6 py-4 transition-transform duration-300">
+      <nav class="navbar glassmorphism fixed z-30 flex w-full items-center justify-between border-b border-b-primary bg-background px-6 py-4 transition-transform duration-300 md:px-10 md:py-6 lg:border-none lg:!bg-transparent lg:px-16 lg:py-8 lg:!shadow-none lg:!backdrop-blur-none">
         <div
           class={`navbar-logo scrollToTop hoverable flex items-end gap-2 ${
             isContentOpen() ? "opacity-0" : "opacity-100"
-          }`}
+          } `}
         >
           <img
+            class="md:w-8"
             src="/assets/logo-transparent.svg"
             alt="butadpj's logo"
             width="22"
           />
-          <span class="navbar-logo-text text-shadow-sm font-k2d text-xl font-bold italic text-primary">
+          <span class="navbar-logo-text text-shadow-sm font-k2d text-xl font-bold italic text-primary md:text-2xl">
             butadpj
           </span>
         </div>
@@ -69,34 +70,35 @@ export default function Navbar() {
           onClick={openNav}
           class="navbar-bars hoverable cursor-pointer rounded-md bg-primary p-2 text-white"
         >
-          <FaSolidBars size={22} />
+          <FaSolidBars class="text-xl md:text-2xl lg:text-3xl" />
         </div>
       </nav>
 
       <Content
-        class="glassmorphism-primary text-2xl text-white"
+        class="glassmorphism-primary"
         isOpen={isContentOpen()}
         closeButtonClick={closeNav}
       >
         <Content.Links links={initLinks()} setActiveLink={setActiveLink} />
-        <Content.Resume class="text-xl" />
+        <Content.Resume />
         <Content.Socials
+          class="text-3xl md:text-5xl"
           socials={[
             {
               href: "https://github.com/butadpj",
-              icon: <FaBrandsSquareGithub size={45} />,
+              icon: <FaBrandsSquareGithub />,
             },
             {
               href: "https://dev.to/butadpj",
-              icon: <FaBrandsDev size={45} />,
+              icon: <FaBrandsDev />,
             },
             {
               href: "https://www.youtube.com/@butadpj",
-              icon: <FaBrandsSquareYoutube size={45} />,
+              icon: <FaBrandsSquareYoutube />,
             },
             {
               href: "https://www.linkedin.com/in/paul-john-butad-5bb70a218",
-              icon: <FaBrandsLinkedin size={45} />,
+              icon: <FaBrandsLinkedin />,
             },
           ]}
         />
